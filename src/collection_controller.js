5a6e6bfb00f676c1413fd65d2f399fb66eaa82d4
Oliver Buchtala <oliver.buchtala@gmail.com>
2014-01-15 21:03:13 +0100

    Preparing document import.

--- src/collection_controller.js
+++ src/collection_controller.js
@@ -5,10 +5,16 @@ var Controller = require("substance-application").Controller;
 var CollectionView = require("./collection_view");
 var util = require("substance-util");
 
+// CollectionController
+// --------
+// options:
+//  - 'import': a function(collectionId, doc, cb) which imports a given document into the collection.
+//
 
-var CollectionController = function(collection) {
+var CollectionController = function(collection, options) {
   Controller.call(this);
   this.collection = collection;
+  this.options = options || {};
 };
 
 CollectionController.Prototype = function() {
@@ -31,6 +37,19 @@ CollectionController.Prototype = function() {
   this.createView = function() {
     if (!this.view) this.view = new CollectionView(this);
     return this.view;
+  };
+
+  this.importDocument = function(doc) {
+    if (this.options["import"]) {
+      this.options["import"](this.collection.id, doc, function(error) {
+        if(error) {
+          console.error(error.message);
+          util.printStackTrace(error, error);
+        }
+      });
+    } else {
+      console.error("Import not supported.");
+    }
   };
 
   // initialize the controller automatically when it is present as a child controller
